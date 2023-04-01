bcef447117b65a794885f1b9fbc4003526763f3b
Gayan Kuruppu <gayan.kuruppu@live.com>
2023-04-01 11:04:31 +0100

    Add Authentication Register/Login/Logout

--- web-app/Controllers/HomeController.cs
+++ web-app/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using web_app.Models;
 
 namespace web_app.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
